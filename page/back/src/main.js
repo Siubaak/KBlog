d1c8b1d063334cd8caf865135a6701b1f08c4f90
Yankun Qiu <q.yk@qq.com>
2017-05-04 14:36:31 +0800

    update for gzip

--- page/back/src/main.js
+++ page/back/src/main.js
@@ -5,8 +5,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './assets/css/main.css'
 
 Vue.config.productionTip = false
